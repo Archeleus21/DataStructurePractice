e57db3eca30f78dbd73f0bc235e5628e50b719cd
Anthony Roybal <Roybal.usmc@gmail.com>
2022-04-02 15:32:28 -0700

    Forward List

---
+++ ForwardList/ForwardList.cpp
@@ -0,0 +1,12 @@
+// ForwardList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
